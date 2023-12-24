8ed5f8e92058d8d060e888a3158ac81ece2299b1
sedep040508 <elikbaevasedeppka@gmail.com>
2023-12-24 11:59:06 +0600

    added file core.js

---
+++ core.js
@@ -0,0 +1,19 @@
+const time = 5000; // ms
+const step = 1;
+
+function outNum(num, elem){
+    let i = document.querySelector('.num');
+    n = 0;
+    let t = Math.round(time/(time/step));
+    let interval = setInterval(()=>{
+        n = n + step;
+        if (n == num){
+            clearInterval(interval);
+        }
+        i.innerHTML = n;
+    },t);
+
+}
+
+outNum(200, 'num');
+
