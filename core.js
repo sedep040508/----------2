48bc74415cc8492076625c7e5ab950682e63ca8e
sedep040508 <elikbaevasedeppka@gmail.com>
2023-12-27 16:28:39 +0600

    added 4 div lit

--- core.js
+++ core.js
@@ -1,18 +1,29 @@
-const time = 90000; // ms
-const step = 1;
+(function(){
 
-function outNum(num, elem){
-    let i = document.querySelector('.num');
-    n = 0;
-    let t = Math.round(time/(time/step));
-    let interval = setInterval(()=>{
-        n = n + step;
-        if (n == num){
-            clearInterval(interval);
+    let counter = document.querySelectorAll('.counter');
+    let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
+    window.addEventListener('scroll', function(){  
+      if( limit == counter.length ){ return; }
+      
+      for(let i = 0; i < counter.length; i++){
+        let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
+        let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
+        if( pos < win && counter[i].dataset.stop === "0" ){
+          counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
+          let x = 0;
+          limit++; // Счетчик будет запущен, увеличиваем переменную на 1
+          let int = setInterval(function(){
+            // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
+            x = x + Math.ceil( counter[i].dataset.to / 50 ); 
+            counter[i].innerText = x;
+            if( x > counter[i].dataset.to ){
+              //Как только досчитали - стираем интервал.
+              counter[i].innerText = counter[i].dataset.to;
+              clearInterval(int);
+            }
+          }, 60);
         }
-        i.innerHTML = n;
-    },t);
+      }
+    });
     
-}
-
-outNum(200, '.num');
+    })();
